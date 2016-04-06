2ad8fde00cb878a1d83edcac2f10661258a0ae67
dib-nhnl <dib_nhnl@163.com>
2016-04-06 23:15:57 +0800

    ppt part 2 edit
    basic bulit

--- js/ppt.js
+++ js/ppt.js
@@ -75,5 +75,80 @@ window.onload = function () {
 	autoplay1();
 
 //box2的图片轮换，下标指定+多层图片立体展示
-
+	var obox2 = document.getElementById('box2');
+	var oli2 = document.getElementById('lists2');
+	var oimg2 = oli2.getElementsByTagName('li');
+	var oc2 = document.getElementById('count2');
+	var on2 = oc2.getElementsByTagName('li');
+	var arr2 = [];
+	for (var i = 0; i < oimg2.length; i++) {
+		var limg = oimg2[i];
+		arr2.push({
+			top: parseInt(getStyle(limg,'top')),
+			left: parseInt(getStyle(limg,'left')),
+			width: parseInt(getStyle(limg,'width')),
+			height: parseInt(getStyle(limg,'height')),
+			zIndex: parseInt(getStyle(limg,'z-index'))
+		});
+		on2[i].index = i;
+		on2[i].onmouseover = function () {
+			show2(this.index);
+		}
+	}
+	// 获取top、left、width、heigh、z-index
+	function getStyle(obj, key) {
+		if (obj.currentStyle) {
+			return obj.currentStyle[key];
+		} else {
+			return getComputedStyle(obj, false)[key];
+		}
+	}
+	//显示图片的函数
+	function show2(n) {
+		//修改包含样式信息的数组内元素顺序
+		//arr2.slice(0)很重要，否则在原数组上操作会混乱
+		var set2 = opArr(n,arr2.slice(0));
+		// 根据修改后的数组操作图片移动
+		for (var i = 0; i < oimg2.length; i++) {
+			oimg2[i].style.zIndex = set2[i].zIndex;
+			move2(oimg2[i],set2[i]);
+		}
+	}
+	// 修改保存了样式的数组的函数
+	function opArr(n,arr) {
+		n = n - Math.floor(oimg2.length / 2);
+		for (var i = 0; i < Math.abs(n); i++) {
+			if (n < 0) {
+				arr.unshift(arr.pop());
+			}
+			if (n > 0) {
+				arr.push(arr.shift());
+			}
+		}
+		return arr;
+	}
+	// 移动图片的函数
+	function move2(obj,arr2item) {
+		clearInterval(obj.timer2);
+		obj.timer2 = setInterval(function () {
+			var de2 = true;
+			for (var key in arr2item) {
+				var cur = parseInt(getStyle(obj, key));
+				var final = arr2item[key];
+				// 设置一个速度，在定时函数内这将制造先快后慢的动画，这除数应该是需要注意的
+				var speed = (final - cur) / 5;
+				speed = (speed >= 0)?Math.ceil(speed):Math.floor(speed);
+				if (cur != final) {
+					de2 = false;
+				}
+				cur = cur + speed;
+				// 什么时候用.key什么时候用[key]取决于key是字符串还是变量，很显然这里key是字符串
+				obj.style[key] = cur + 'px';
+			}
+			if (de2 == true) {
+				clearInterval(obj.timer2);
+				obj.timer2 = null;
+			}
+		},50);
+	}
 }
